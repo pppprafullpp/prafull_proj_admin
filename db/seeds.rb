22ba56670c1e887b1b60bf13cfa36f9ae36e6995
apoorv <apoorv.coder@live.com>
2016-09-27 12:42:43 +0530

    deal CRUD feature complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ admin.save
 admin = Admin.find_or_initialize_by(:name => 'Ram',:email => 'ram.garg@spa-systems.com')
 admin.password = Admin.encrypt('123456')
 admin.save
+admin = Admin.find_or_initialize_by(:name => 'Apoorv',:email => 'apoorv@sp-assurance.com')
+admin.password = Admin.encrypt('123456')
+admin.save
