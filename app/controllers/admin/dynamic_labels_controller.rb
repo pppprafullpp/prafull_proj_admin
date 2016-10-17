9f707a2e12c93bc6d17ad60b0a3c3c33d81dc85e
apoorv <apoorv.coder@live.com>
2016-10-17 15:34:17 +0530

    17th oct review changes

--- app/controllers/admin/dynamic_labels_controller.rb
+++ app/controllers/admin/dynamic_labels_controller.rb
@@ -4,14 +4,14 @@ class Admin::DynamicLabelsController < Admin::AdminApplicationController
   def index
     params[:search] = {} unless params[:search].present?
     @search = DynamicLabel.new
-    @dynamic_label = DynamicLabel.new
     @breadcrumb = {'Home' => home_url, 'Dynamic Labels' => ''}
-    @dynamic_labels = DynamicLabel.paginate(:page => params[:page], :per_page => PER_PAGE)
+    @dynamic_labels = DynamicLabel.search(params[:search]).paginate(:page => params[:page], :per_page => PER_PAGE)
   end
 
   def create
-    if params[:dynamic_labels] && params[:dynamic_labels][:id].present?
-      DynamicLabel.find(params[:dynamic_labels][:id]).update_attributes(:label_key => params[:dynamic_labels][:label_key],:label_value => params[:dynamic_labels][:label_value],:service_provider_id => params[:dynamic_labels][:service_provider_id])
+    # raise params.to_yaml
+    if params[:dynamic_label][:id].present?
+      DynamicLabel.find(params[:dynamic_label][:id]).update_attributes(:label_key => params[:dynamic_label][:label_key],:label_value => params[:dynamic_label][:label_value],:service_provider_id => params[:dynamic_label][:service_provider_id], :status => params[:dynamic_label][:status])
     else
       @dynamic_label = DynamicLabel.new(dynamic_label_params)
       @dynamic_label.save!
